50c88c55af10d80c9108a653ba93b1b3ff8f7102
Laura Kim <Doyeunkim120@gmail.com>
2022-03-08 17:18:33 -0800

    css styling and portfolio content added to carousel

--- src/Footer.js
+++ src/Footer.js
@@ -9,9 +9,14 @@ export default function Footer() {
     return (
         <div className="Footer">
             <div className="contact-links">
-                <a href="#"><FontAwesomeIcon icon={faLinkedinIn} className="fa-xl" /></a>
-                <a href="mailto:Laurakim120@gmail.com?subject=Reaching out from your portfolio"><FontAwesomeIcon icon={faEnvelope} className="fa-xl" /></a>
+                <div className="link-items">
+                <a href="#"><FontAwesomeIcon icon={faLinkedinIn} className="fa-lg" /></a>
+                <a href="mailto:Laurakim120@gmail.com?subject=Reaching out from your portfolio"><FontAwesomeIcon icon={faEnvelope} className="fa-lg" /></a>
                 <span><FontAwesomeIcon icon={faPhone} className="search-icon" /> 847) 791 7503</span>
+                </div>
+                <div className="git-hub-link">
+                <p className="open-source"><a href="https://github.com/LDK0120/final-portfolio" target="_blank">open-source</a> coded by Laura Kim</p>
+                </div>
             </div>
         </div>
     )
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
     const [position, setPosition] = useState(0)
@@ -24,15 +26,27 @@ export default function Home() {
             <div className="carousel">
 
         <div className="move-carousel-location" style={{ transform: `translateX(-${position * 100}%)` }}>
-            <div className="carousel-item" style={ { width: "100%" }}>Item 1</div>
-            <div className="carousel-item" style={ { width: "100%" }}>Item 2</div>
-            <div className="carousel-item" style={ { width: "100%" }}>Item 3</div>
-            <div className="carousel-item" style={ { width: "100%" }}>Item 4</div>
+            <div className="carousel-item" style={ { width: "100%" }}>
+                <div className="item-image item-image-one"></div>
+                <div className="item-content item-content-one">contents</div>
+            </div>
+            <div className="carousel-item" style={ { width: "100%" }}>
+                <div className="item-image item-image-two"></div>
+                <div className="item-content item-content-two">contents</div>
+            </div>
+            <div className="carousel-item" style={ { width: "100%" }}>
+                <div className="item-image item-image-three"></div>
+                <div className="item-content item-content-three">contents</div>
+            </div>
+            <div className="carousel-item" style={ { width: "100%" }}>
+                <div className="item-image item-image-four"></div>
+                <div className="item-content item-content-four">contents</div>
+            </div>
         </div>
 
         <div className="buttons">
-            <button className="prev btn" id={-1} onClick={handleClick}>prev</button>
-            <button className="next btn" id={1} onClick={handleClick}>next</button>
+            <button className="prev btn" id={-1} onClick={handleClick}><FontAwesomeIcon icon={faAngleLeft} /></button>
+            <button className="next btn" id={1} onClick={handleClick}><FontAwesomeIcon icon={faAngleRight} /></button>
         </div>
 
             </div>
